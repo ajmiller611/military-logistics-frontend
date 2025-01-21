d89f3abac19f72a1057b22ab3677d209ed0126d7
ajmiller611 <ajmiller611@live.com>
2025-01-21 15:34:23 -0500

    Added user registration page

---
+++ src/app/users/register/page.tsx
@@ -0,0 +1,53 @@
+'use client';
+import { Box } from '@mui/material';
+import { useState } from 'react';
+import RegisterUserForm from '@/components/RegisterUserForm';
+import { UserInput } from '@/schemas/userSchema';
+import axiosInstance from '@/lib/axiosInstance';
+import axios from 'axios';
+
+const apiEndpoint = '/users/';
+
+export default function RegisterUserPage() {
+  const [apiResponse, setApiResponse] = useState<Record<string, string>>({});
+  const handleUserSubmit = async (data: UserInput) => {
+    console.log('User data submitted: ', data);
+    setApiResponse({}); // Clear previous errors
+    try {
+      const response = await axiosInstance.post(apiEndpoint, data);
+      console.log('Response:', response);
+      if (response.status === 201) {
+        console.log('User created successfully: ', response.data);
+        setApiResponse({ success: 'User created successfully!' });
+      }
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          switch (error.response.status) {
+            case 409: // Conflict
+              console.error('User already exists: ', error.response.data);
+              setApiResponse({ username: error.response.data.message });
+              break;
+            case 400: // Bad Request
+              console.error('Invalid input: ', error.response.data);
+              alert('Invalid input. Please check your data and try again.');
+              break;
+            default:
+              console.error('An error occurred: ', error.response.data);
+              break;
+          }
+        } else {
+          console.error('No response received: ', error.message);
+        }
+      } else {
+        console.error('An unexpected error occurred: ', error);
+      }
+    }
+  };
+
+  return (
+    <Box>
+      <RegisterUserForm onSubmit={handleUserSubmit} apiResponse={apiResponse} />
+    </Box>
+  );
+}
